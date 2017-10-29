a6e1a976ae8e0c10a7e34a8ded935e5aff872c51
Daniel Quinn <code@danielquinn.org>
2017-10-29 18:50:34 +0000

    Add Python library

--- python/aletheia.py
+++ python/aletheia.py
@@ -3,7 +3,7 @@
 #
 # Python API:
 #
-#   from alethia import generate, sign, verify
+#   from aletheia import generate, sign, verify
 #
 #   generate(path)
 #   sign(path, key, public_key_url)
@@ -20,58 +20,30 @@ import argparse
 import base64
 import os
 import subprocess
+import sys
+from hashlib import sha512
 
-from six import text_type
+import requests
 
+from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
-from cryptography.hazmat.primitives.asymmetric.rsa import (
-    RSAPrivateKey,
-    RSAPublicKey
-)
 
 
 class Aletheia:
 
     KEY_SIZE = 8192
-
-    def __init__(self, private_key=None, public_key=None):
-
-        self.private_key = None
-        self.public_key = None
-
-        if private_key:
-            self._init_private_key(private_key)
-
-        if public_key:
-            self._init_public_key(public_key)
-
-    def _init_private_key(self, key):
-        if isinstance(key, RSAPrivateKey):
-            self.private_key = key
-        elif isinstance(key, text_type):
-            if "BEGIN RSA PRIVATE KEY" in key.split("\n")[0]:
-                self.private_key = serialization.load_pem_private_key(
-                     key,
-                     password=None,
-                     backend=default_backend()
-                )
-        raise RuntimeError("The private key is not in a recognisable format")
-
-    def _init_public_key(self, key):
-        if isinstance(key, RSAPublicKey):
-            self.public_key = key
-        elif isinstance(key, text_type):
-            if "BEGIN PUBLIC KEY" in key.split("\n")[0]:
-                self.public_key = serialization.load_pem_public_key(
-                     key,
-                    backend=default_backend()
-                )
-        raise RuntimeError("The public key is not in a recognisable format")
+    HOME = os.path.join(os.getenv("HOME"), ".aletheia")
+    PRIVATE_KEY_PATH = os.path.join(HOME, "aletheia.pem")
+    PRIVATE_KEY_NAME = "ALETHEIA_PRIVATE_KEY"
+    PUBLIC_KEY_CACHE = os.path.join(HOME, "public-keys")
 
     @classmethod
-    def generate(cls, path):
+    def generate(cls):
+
+        path = cls.HOME
+        os.makedirs(cls.HOME, exist_ok=True)
 
         private_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -103,18 +75,18 @@ class Aletheia:
                 stdin=f
             )
 
-        signature = base64.encodebytes(self.private_key.sign(
+        signature = base64.encodebytes(self._get_private_key().sign(
             image_data,
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()),
                 salt_length=padding.PSS.MAX_LENGTH
             ),
             hashes.SHA256()
-        ))
+        )).strip()
 
         subprocess.call((
             "exiftool",
-            "-ImageSupplierImageID={}".format(signature),
+            "-ImageSupplierImageID={}".format(signature.decode("utf-8")),
             "-overwrite_original",
             path
         ))
@@ -131,17 +103,22 @@ class Aletheia:
             raise FileNotFoundError("Specified file doesn't exist")
 
         with open(path, "rb") as f:
-            signature = subprocess.check_output(
-                ("exiftool", "-s", "-s", "-s", "-ImageSupplierImageID"),
+            key_url = subprocess.check_output(
+                ("exiftool", "-s", "-s", "-s", "-ImageSupplierID", "-"),
                 stdin=f
-            )
+            ).strip()
+            f.seek(0)
+            signature = base64.decodebytes(subprocess.check_output(
+                ("exiftool", "-s", "-s", "-s", "-ImageSupplierImageID", "-"),
+                stdin=f
+            ).strip())
             f.seek(0)
             image_data = subprocess.check_output(
                 ("exiftool", "-all=", "-"),
                 stdin=f
             )
 
-        self.public_key.verify(
+        self._get_public_key(key_url).verify(
             signature,
             image_data,
             padding.PSS(
@@ -151,41 +128,153 @@ class Aletheia:
             hashes.SHA256()
         )
 
+        return True
 
-def generate(path):
-    Aletheia.generate(path)
+    def _get_private_key(self):
+        """
+        Try to set the private key by either (a) pulling it from the
+        environment, or (b) sourcing it from a file in a known location.
+        """
+
+        environment_key = os.getenv("ALETHEIA_PRIVATE_KEY")
+        if environment_key:
+            environment_key = bytes(environment_key.encode("utf-8"))
+            if b"BEGIN RSA PRIVATE KEY" in environment_key.split(b"\n")[0]:
+                return serialization.load_pem_private_key(
+                     environment_key,
+                     password=None,
+                     backend=default_backend()
+                )
+
+        if os.path.exists(self.PRIVATE_KEY_PATH):
+            with open(self.PRIVATE_KEY_PATH, "rb") as f:
+                return serialization.load_pem_private_key(
+                     f.read(),
+                     password=None,
+                     backend=default_backend()
+                )
+
+        raise RuntimeError(
+            "You don't have a private key defined, so signing is currently "
+            "impossible.  Either generate a key and store it at {} or put the "
+            "key into an environment variable called {}.".format(
+                self.PRIVATE_KEY_PATH,
+                self.PRIVATE_KEY_NAME
+            )
+        )
+
+    def _get_public_key(self, url):
+
+        os.makedirs(self.PUBLIC_KEY_CACHE, exist_ok=True)
+
+        cache = os.path.join(self.PUBLIC_KEY_CACHE, sha512(url).hexdigest())
+        if os.path.exists(cache):
+            with open(cache, "rb") as f:
+                return serialization.load_pem_public_key(
+                     f.read(),
+                     backend=default_backend()
+                )
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            if b"BEGIN PUBLIC KEY" in response.content:
+                with open(cache, "wb") as f:
+                    f.write(requests.get(url).content)
+                return self._get_public_key(url)
+
+        raise RuntimeError("The specified URL does not contain a public key")
 
 
-def sign(path, key, public_key_url):
-    Aletheia(private_key=key).sign(path, public_key_url)
+def generate():
+    Aletheia.generate()
 
 
-def verify(path, key):
-    Aletheia(public_key=key).verify(path)
+def sign(path, public_key_url):
+    Aletheia().sign(path, public_key_url)
+
+
+def sign_bulk(paths, public_key_url):
+    aletheia = Aletheia()
+    for path in paths:
+        aletheia.sign(path, public_key_url)
+
+
+def verify(path):
+    Aletheia().verify(path)
+
+
+def verify_bulk(paths):
+    aletheia = Aletheia()
+    for path in paths:
+        aletheia.verify(path)
 
 
 class Command:
 
     def __init__(self):
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument("command", choices=("generate", "sign", "verify"))
-        parser.add_argument("path")
-        parser.parse_args()
+        parser = argparse.ArgumentParser(prog="aletheia")
+        parser.set_defaults(func=parser.print_help)
+        subparsers = parser.add_subparsers(dest="subcommand")
+
+        subparsers.add_parser(
+            "generate",
+            help="Generate a public/private key pair for use in siging & 3rd "
+                 "party verification. (Do this first)"
+        )
+
+        parser_sign = subparsers.add_parser("sign", help="Sign a file")
+        parser_sign.add_argument("path")
+        parser_sign.add_argument("url")
+
+        parser_verify = subparsers.add_parser(
+            "verify", help="Verify the origin of a file")
+        parser_verify.add_argument("path")
 
         args = parser.parse_args()
 
-        getattr(self, args.command)(args.path)
+        try:
+            if args.subcommand:
+                getattr(self, args.subcommand)(args)
+            else:
+                parser.print_help()
+        except (RuntimeError, FileNotFoundError) as e:
+            print(e, file=sys.stdout)
+            sys.exit(1)
 
-    def generate(self, path):
-        print("Generating private/public key pair")
-        Aletheia.generate(path)
+        sys.exit(0)
 
-    def sign(self, path):
-        pass
+    @classmethod
+    def generate(cls, *args):
+        print("Generating private/public key pair...")
+        generate()
+        print("""
+            All finished!
+            
+            You now have two files: aletheia.pem (your private key) and
+            aletheia.pub (your public key).  Keep the former private, and share
+            the latter far-and-wide.  Importantly, place your public key at a
+            publicly accessible URL so that when you sign a file with your
+            private key, it can be verified by reading the public key at that
+            URL.
+        """.replace("            ", ""))
 
-    def verify(self, path):
-        pass
+    @classmethod
+    def sign(cls, args):
+        print("Signing {} with your private key".format(args.path))
+        sign(args.path, args.url)
+        sys.exit(0)
+
+    @classmethod
+    def verify(cls, args):
+        try:
+            verify(args.path)
+        except InvalidSignature:
+            print("There's something wrong with that file")
+            sys.exit(1)
+
+        print("The file appears to be legit")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
